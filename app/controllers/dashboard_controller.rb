21177b0cee1ecec3bd3ad3cb7c2aaeb6eff1fed3
Kunj-Choksi <kunj07soni@gmail.com>
2022-02-20 22:38:39 -0500

    Added field in dashboard data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,9 @@ class DashboardController < ApplicationController
         map = {}
 
         map['overall'] = transactions
-        map['this_week'] = transactions.by_week
-        map['this_month'] = transactions.by_month
-        map['this_month_spending'] = transactions.by_month.sum(:amount)
+        map['this_week'] = transactions.by_week(field: :datetime)
+        map['this_month'] = transactions.by_month(field: :datetime)
+        map['this_month_spending'] = transactions.by_month(field: :datetime).sum(:amount)
         render_result_json map
     end
 end
