4c2387a99db6c88578f7314115d1bf8e8c36276c
Kunj-Choksi <kunj07soni@gmail.com>
2022-03-13 12:13:08 -0400

    Added Stores list

--- app/controllers/master/store_controller.rb
+++ app/controllers/master/store_controller.rb
@@ -1,8 +1,8 @@
 class Master::StoreController < ApplicationController
-  
+  layout 'dashboard'
   before_action :check_for_logged_in_session
-  
+
   def index
-    
+    @stores = Master::Store.all
   end
 end
