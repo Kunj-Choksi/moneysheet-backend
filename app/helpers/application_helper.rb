ffd6dd5c77558f20b977030f58cd2647ddfae160
Kunj-Choksi <kunj07soni@gmail.com>
2022-04-04 12:40:02 -0400

    Gravatar and fixed login page design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_image
+    email_address = current_user.present? ? current_user.email : ""
+    hash = Digest::MD5.hexdigest(current_user.email)
+    image_tag("https://www.gravatar.com/avatar/#{hash}", :class => "h-8 w-8 rounded-full")
+  end
 end
